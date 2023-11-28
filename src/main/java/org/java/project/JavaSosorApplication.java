2140d376e405b5c78be19f6d553afdb5f2f6dfdf
Luigi213 <reyesgianluigi88@gmail.com>
2023-11-28 12:47:29 +0100

    init commit

---
+++ src/main/java/org/java/project/JavaSosorApplication.java
@@ -0,0 +1,13 @@
+package org.java.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSosorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSosorApplication.class, args);
+	}
+
+}
